d42c3f8c01a6e61ddf93f5f3bcf21a2baf05acb6
ReallyPhyla <69743753+ReallyJack@users.noreply.github.com>
2024-09-24 06:40:22 +0930

    Add files via upload

---
+++ src/main/java/com/example/ttf/Product.java
@@ -0,0 +1,37 @@
+package com.example.ttf;
+
+import androidx.annotation.Keep;
+
+public class Product {
+    private String id;
+    private String name;
+    private int stockCount;
+
+    @Keep
+    public Product() {
+        // Default constructor required for calls to DataSnapshot.getValue(Product.class)
+    }
+
+    public Product(String id, String name, int stockCount) {
+        this.id = id;
+        this.name = name;
+        this.stockCount = stockCount;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getStockCount() {
+        return stockCount;
+    }
+
+    public void setStockCount(int stockCount) {
+        this.stockCount = stockCount;
+    }
+}
+
